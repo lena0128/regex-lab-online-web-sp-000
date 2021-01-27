c5530ed431a7cd276c34014324bf654071810f92
Lena Hou <lenahou2013@gmail.com>
2021-01-27 18:01:21 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,8 +1,8 @@
 def starts_with_a_vowel?(word)
-  if word.match(/\A+[aeiouAEIUO]/)
-    true
-  else
+  if word.match(/\A+[aeiouAEIUO]/) == nil
     false
+  else
+    true
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
