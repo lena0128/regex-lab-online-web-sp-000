b5cafa857c712d8605c190d92f18151270c3d824
Lena Hou <lenahou2013@gmail.com>
2021-01-27 18:30:49 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if /\[0-9]{10}/.match(phone) == nil
+  if /([0-9] *?){10}/.match(phone) == nil
     return false
   else
     return true
