ad5e7cfd1ebfffd6f338e76df7b340db50a34c65
Lena Hou <lenahou2013@gmail.com>
2021-01-27 18:07:22 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,10 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.match(/^[aeiouAEIUO]/+\w/) then
-    return true
-  else
-    false
-  end
- end
+
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
