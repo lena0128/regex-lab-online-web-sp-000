7bdf21ba245c17308b38796d00aca8940aca741f
Lena Hou <lenahou2013@gmail.com>
2021-01-27 18:33:14 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,31 @@
 def starts_with_a_vowel?(word)
-
+  if /^[aeiouAEIOU]+\w/.match(word) == nil
+    return false
+  else
+    return true
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan(/un+\w+ing\b/)
 end
 
 def words_five_letters_long(text)
-
+text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+   if /^[A-Z].+[\.!?]$/.match(text) == nil
+    return false
+  else
+    return true
+  end
 end
 
 def valid_phone_number?(phone)
-
+  if /([0-9] *?){10}|(\([0-9]{3}\)(([0-9]{3}-[0-9]{4})|[0-9]{7})\b)/.match(phone) == nil
+    return false
+  else
+    return true
+  end
 end
